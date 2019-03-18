3520c104994794105e4decdeeb3d4e6a2d4d7869
Yevgeniy Bespal <yevbes@yevbes.com>
2019-03-18 10:50:54 +0100

    fix interceptor

--- app/src/main/java/com/yevbes/movieland/network/interceptors/HeaderInterceptor.kt
+++ app/src/main/java/com/yevbes/movieland/network/interceptors/HeaderInterceptor.kt
@@ -9,6 +9,9 @@ class HeaderInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val original = chain.request()
 
+        if (original.url().encodedPath().equals("/${AppConfig.API_VERSION}/auth/request_token",true) && original.method().equals("post",true)) {
+            return chain.proceed(original)
+        }
         val requestBuilder = original.newBuilder()
             .header("Authorization", PreferencesManager.getAuthToken()!!)
             .header("Content-Type", AppConfig.CONTENT_TYPE)
